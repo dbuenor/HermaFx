77814d53b3da9c9d79726592137c642de0e12916
David Bueno <dbueno@evicertia.com>
2016-04-25 15:29:49 +0200

    Added Orders db as json file (@7h)

---
+++ HermaFx.Mvc.Site/Models/Database.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using System.Xml.Linq;
+
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Web.Http.Routing;
+
+namespace HermaFx.Mvc.Site.Models
+{
+	public class Database
+	{
+		public static IQueryable<Order> GetOrders()
+		{
+			using (var reader = new StreamReader(HttpContext.Current.Server.MapPath("~/Content/Json/Orders.json")))
+			{
+				var test = reader.ReadToEnd();
+				var res = JsonConvert.DeserializeObject<List<Order>>(test);
+
+				return res.AsQueryable();
+			}
+		}
+	}
+}
--- HermaFx.Mvc.Site/Models/OrdersRepository.cs
+++ HermaFx.Mvc.Site/Models/OrdersRepository.cs
@@ -11,10 +11,8 @@ namespace HermaFx.Mvc.Site.Models
 
 		public override IOrderedQueryable<Order> GetAll()
 		{
-			// FIXME: Change Db Source or Add Sql Server 2012 Express Local Db to solution
-			// Another option is to use MongoDb driver and use this db:
-			// https://github.com/tmcnab/northwind-mongo
-			return Enumerable.Empty<Order>().AsQueryable().OrderByDescending(o => o.OrderDate);
+			return Database.GetOrders().OrderByDescending(o => o.OrderDate);
+
 			//return EfDbSet.Include("Customer").OrderByDescending(o => o.OrderDate);
 		}
 
